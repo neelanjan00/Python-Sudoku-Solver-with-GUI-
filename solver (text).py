7f4bf2501f637f07084e8f7440c06fcea5f21342
Neelanjan Manna <neelanjanmanna@gmail.com>
2020-01-10 19:14:18 +0530

    Update solver (text).py

--- solver (text).py
+++ solver (text).py
@@ -1,4 +1,16 @@
 # solver.py
+board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
 def solve(bo):
     """
     Solves a sudoku board using backtracking
@@ -88,3 +100,10 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
+
+print("\nInput board:\n")
+print_board(board)
+solve(board)
+print("\nSolved board:\n")
+print_board(board)
+print("\n")
